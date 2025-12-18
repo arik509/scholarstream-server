a75d3362d2141cf1c42ceba502be53c8704a1df1
arik509 <sabirhossainarik34@gmail.com>
2025-12-19 03:48:05 +0600

    Added Backend Routes for User Management

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion,ObjectId  } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
@@ -26,10 +26,9 @@ async function run() {
 
     const db = client.db(process.env.DB_NAME);
     const usersCollection = db.collection("users");
-    const scholarshipsCollection = db.collection('scholarships');
-    const applicationsCollection = db.collection('applications');
-    const reviewsCollection = db.collection('reviews');
-
+    const scholarshipsCollection = db.collection("scholarships");
+    const applicationsCollection = db.collection("applications");
+    const reviewsCollection = db.collection("reviews");
 
     app.get("/api/users/:email", async (req, res) => {
       const email = req.params.email;
@@ -52,49 +51,80 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/api/scholarships", async (req, res) => {
+      const scholarships = await scholarshipsCollection.find().toArray();
+      res.send(scholarships);
+    });
 
-    app.get('/api/scholarships', async (req, res) => {
-        const scholarships = await scholarshipsCollection.find().toArray();
-        res.send(scholarships);
+    app.get("/api/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const scholarship = await scholarshipsCollection.findOne({
+        _id: new ObjectId(id),
       });
+      res.send(scholarship);
+    });
 
-      app.get('/api/scholarships/:id', async (req, res) => {
-        const id = req.params.id;
-        const scholarship = await scholarshipsCollection.findOne({ _id: new ObjectId(id) });
-        res.send(scholarship);
+    app.post("/api/scholarships", async (req, res) => {
+      const scholarship = req.body;
+      const result = await scholarshipsCollection.insertOne(scholarship);
+      res.send(result);
+    });
+
+    app.get("/api/applications", async (req, res) => {
+      const applications = await applicationsCollection.find().toArray();
+      res.send(applications);
+    });
+
+    app.get("/api/applications/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const applications = await applicationsCollection
+        .find({ userEmail: email })
+        .toArray();
+      res.send(applications);
+    });
+
+    app.post("/api/applications", async (req, res) => {
+      const application = req.body;
+      const result = await applicationsCollection.insertOne(application);
+      res.send(result);
+    });
+
+    app.get("/api/reviews/scholarship/:id", async (req, res) => {
+      const scholarshipId = req.params.id;
+      const reviews = await reviewsCollection.find({ scholarshipId }).toArray();
+      res.send(reviews);
+    });
+
+    app.post("/api/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+
+
+    app.get('/api/users', async (req, res) => {
+        const users = await usersCollection.find().toArray();
+        res.send(users);
       });
-
-      app.post('/api/scholarships', async (req, res) => {
-        const scholarship = req.body;
-        const result = await scholarshipsCollection.insertOne(scholarship);
-        res.send(result);
-      });
-
-      app.get('/api/applications', async (req, res) => {
-        const applications = await applicationsCollection.find().toArray();
-        res.send(applications);
-      });
-
       
-    app.get('/api/applications/user/:email', async (req, res) => {
+      app.patch('/api/users/:email/role', async (req, res) => {
         const email = req.params.email;
-        const applications = await applicationsCollection.find({ userEmail: email }).toArray();
-        res.send(applications);
-      });
-  
-
-      app.post('/api/applications', async (req, res) => {
-        const application = req.body;
-        const result = await applicationsCollection.insertOne(application);
+        const { role } = req.body;
+        
+        const result = await usersCollection.updateOne(
+          { email },
+          { $set: { role } }
+        );
+        
         res.send(result);
       });
-
-      app.post('/api/reviews', async (req, res) => {
-        const review = req.body;
-        const result = await reviewsCollection.insertOne(review);
+      
+      app.delete('/api/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const result = await usersCollection.deleteOne({ email });
         res.send(result);
       });
-  
+      
 
     await client.db("admin").command({ ping: 1 });
     console.log(
